40146338f8e1c99f8bd640d3363eb1b22efe43ed
jin128 <hitoshi.jin.yamamoto@gmail.com>
2022-09-20 15:47:40 +0900

    Update test.js

--- js/test.js
+++ js/test.js
@@ -1,1 +1,29 @@
+let a = function() {
+  const bolb = new Blob(['hoge', { type: 'text/plain'});
+  let aEl = document.createElement('a');
+  aEl.href = window.URL.createdObjectURL(blob)
+  
+  aEl.download = 'test.txt';
+  // aEl.setAttribute('download', 'test.txt')
+  
+  document.body.appebdChild(aEl);
+  
+  aEl.click()
+  
+  document.body.removeChild(aEl);
+}
 
+let b = function() {
+  const bolb = new Blob(['hoge', { type: 'text/plain'});
+  let aEl = document.createElement('a');
+  aEl.href = window.URL.createdObjectURL(blob)
+  
+  //aEl.download = 'test.txt';
+  aEl.setAttribute('download', 'test.txt')
+  
+  document.body.appebdChild(aEl);
+  
+  aEl.click()
+  
+  document.body.removeChild(aEl);
+}
